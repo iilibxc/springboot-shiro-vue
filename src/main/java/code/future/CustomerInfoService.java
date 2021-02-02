9dd011787ad54c0c7b84df65bb82f3fc30a2e014
iilibxc <2026262766@qq.com>
2021-02-02 10:27:59 +0800

    delete

--- src/main/java/code/future/CustomerInfoService.java
+++
@@ -1,7 +0,0 @@
-package code.future;
-public class CustomerInfoService implements RemoteLoader {
-    public String load() {
-        this.delay();
-        return "基本信息";
-    }
-}
--- src/main/java/code/future/LearnRecordService.java
+++
@@ -1,8 +0,0 @@
-package code.future;
-
-public class LearnRecordService implements RemoteLoader {
-    public String load() {
-        this.delay();
-        return "学习信息";
-    }
-}
